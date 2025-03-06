e1321135e318d84d16108331549761386850807e
Motalib01 <abdelmotalib.chemouri@gmail.com>
2025-03-07 00:18:25 +0100

    Add Packages

--- Shopily.Api/Program.cs
+++ Shopily.Api/Program.cs
@@ -7,6 +7,7 @@ internal class Program
 
         // Add services to the container.
 
+
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
